9abe5c96d440e8fc2cbf92c5cb240b4d9bb878b6
widialfrda <widialfrda@gmail.com>
2021-06-05 03:15:57 +0700

    update

--- app/src/main/java/com/example/nonawn/Common/SignUpLogin/OTPVerify.java
+++ app/src/main/java/com/example/nonawn/Common/SignUpLogin/OTPVerify.java
@@ -8,42 +8,40 @@ import android.view.View;
 
 import com.chaos.view.PinView;
 import com.example.nonawn.R;
-import com.google.android.material.textfield.TextInputLayout;
 
 public class OTPVerify extends AppCompatActivity {
 
-    TextInputLayout otp_verify;
+    PinView otp_verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verify);
 
-        //variable for otp
         otp_verify = findViewById(R.id.otp_pinview);
     }
 
-    private boolean validateOTPPass(){
-        String val = otp_verify.getEditText().getText().toString().trim();
-        String checkOTP = "^"+"(?=.*[0-9])"+".{4,}";
+    private boolean validateOTPpass(){
+        String val = otp_verify.getText().toString().trim();
+        String check_otp = "^"+"(?=.*[0-9])"+".{4,}";
 
         if (val.isEmpty()){
             otp_verify.setError("Harus diisi");
             return false;
         }
-        else if(!val.matches(checkOTP)){
-            otp_verify.setError("Isi harus berupa 4 angka");
+        else if(!val.matches(check_otp)){
+            otp_verify.setError("Harus mengandung 4 angka");
             return false;
         }
-        else{
-            otp_verify.setError(null);
-            otp_verify.setErrorEnabled(false);
+        else if(val.matches(check_otp)){
             return true;
         }
+        else {
+            return true;
+        }
+
     }
-
     public void submitOTP(View view) {
-
         startActivity(new Intent(getApplicationContext(),SetNewPass.class));
         finish();
     }
